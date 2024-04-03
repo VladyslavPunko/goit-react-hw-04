e3aeb92cd0a4f6e3c53f3bdce8b277d39735c134
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-03 10:07:57 +0200

    1

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -1,10 +1,10 @@
-import cat from "../CatError.png"
+
 import css from "./ErrorMessage.module.css"
 
 const ErrorMessage = () => {
   return (<div className={css.wrap}>    
     <p className={css.text}>Woooops... Something went wrong. Please reload the page</p>
-    <img className={css.imge} src={cat} alt="cat" /></div>
+   </div>
 
   )
 }
